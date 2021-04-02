4c2c1198131e366c2b24ab5b78da43f4f24d563f
Saverio Vigni <s.vigni@hor-net.com>
2021-04-02 18:23:26 +0200

    added arbitrary number of channels handling for ProcessBlock

--- IPlug/Extras/Oversampler.h
+++ IPlug/Extras/Oversampler.h
@@ -17,7 +17,6 @@
 
 #include "HIIR/FPUUpsampler2x.h"
 #include "HIIR/FPUDownsampler2x.h"
-//#include "HIIR/PolyphaseIIR2Designer.h"
 
 #include "heapbuf.h"
 #include "ptrlist.h"
@@ -49,6 +48,12 @@ public:
   , mNInChannels(nInChannels)
   , mNOutChannels(nOutChannels)
   {
+    
+    static constexpr double coeffs2x[12] = { 0.036681502163648017, 0.13654762463195794, 0.27463175937945444, 0.42313861743656711, 0.56109869787919531, 0.67754004997416184, 0.76974183386322703, 0.83988962484963892, 0.89226081800387902, 0.9315419599631839, 0.96209454837808417, 0.98781637073289585 };
+    static constexpr double coeffs4x[4] = {0.041893991997656171, 0.16890348243995201, 0.39056077292116603, 0.74389574826847926 };
+    static constexpr double coeffs8x[3] = {0.055748680811302048, 0.24305119574153072, 0.64669913119268196 };
+    static constexpr double coeffs16x[2] = {0.10717745346023573, 0.53091435354504557 };
+
     for (auto c = 0; c < mNInChannels; c++)
     {
       mUpsampler2x.Add(new Upsampler2xFPU<12, T>());
@@ -56,21 +61,13 @@ public:
       mUpsampler8x.Add(new Upsampler2xFPU<3, T>());
       mUpsampler16x.Add(new Upsampler2xFPU<2, T>());
       
-      static constexpr double coeffs2x[12] = { 0.036681502163648017, 0.13654762463195794, 0.27463175937945444, 0.42313861743656711, 0.56109869787919531, 0.67754004997416184, 0.76974183386322703, 0.83988962484963892, 0.89226081800387902, 0.9315419599631839, 0.96209454837808417, 0.98781637073289585 };
-
       mUpsampler2x.Get(c)->set_coefs(coeffs2x);
-      
-      static constexpr double coeffs4x[4] = {0.041893991997656171, 0.16890348243995201, 0.39056077292116603, 0.74389574826847926 };
-  
       mUpsampler4x.Get(c)->set_coefs(coeffs4x);
-  
-      static constexpr double coeffs8x[3] = {0.055748680811302048, 0.24305119574153072, 0.64669913119268196 };
-    
       mUpsampler8x.Get(c)->set_coefs(coeffs8x);
-  
-      static constexpr double coeffs16x[2] = {0.10717745346023573, 0.53091435354504557 };
-  
       mUpsampler16x.Get(c)->set_coefs(coeffs16x);
+      
+      // ptr location doesn't matter at this stage
+      mNextInputPtrs.Add(mUp2x.Get());
     }
     
     for (auto c = 0; c < mNOutChannels; c++)
@@ -80,33 +77,15 @@ public:
       mDownsampler8x.Add(new Downsampler2xFPU<3, T>());
       mDownsampler16x.Add(new Downsampler2xFPU<2, T>());
       
-      static constexpr double coeffs2x[12] = { 0.036681502163648017, 0.13654762463195794, 0.27463175937945444, 0.42313861743656711, 0.56109869787919531, 0.67754004997416184, 0.76974183386322703, 0.83988962484963892, 0.89226081800387902, 0.9315419599631839, 0.96209454837808417, 0.98781637073289585 };
-      
       mDownsampler2x.Get(c)->set_coefs(coeffs2x);
-      
-      static constexpr double coeffs4x[4] = {0.041893991997656171, 0.16890348243995201, 0.39056077292116603, 0.74389574826847926 };
-      
       mDownsampler4x.Get(c)->set_coefs(coeffs4x);
-      
-      static constexpr double coeffs8x[3] = {0.055748680811302048, 0.24305119574153072, 0.64669913119268196 };
-      
       mDownsampler8x.Get(c)->set_coefs(coeffs8x);
-      
-      static constexpr double coeffs16x[2] = {0.10717745346023573, 0.53091435354504557 };
-      
       mDownsampler16x.Get(c)->set_coefs(coeffs16x);
-    }
-    
-    for (auto c = 0; c < mNInChannels; c++)
-    {
-      mNextInputPtrs.Add(mUp2x.Get()); // ptr location doesn't matter at this stage
-    }
-    
-    for (auto c = 0; c < mNOutChannels; c++)
-    {
+      
+      // ptr location doesn't matter at this stage
       mNextOutputPtrs.Add(mDown2x.Get());
     }
-    
+        
     SetOverSampling(factor);
     
     Reset();
@@ -226,65 +205,44 @@ public:
       mPrevRate = mRate;
     }
 
-    if (mRate >= 2) {
-      for(auto c = 0; c < nInChans; c++) {
+    for(auto c = 0; c < nInChans; c++) {
+      if (mRate >= 2) {
         mUpsampler2x.Get(c)->process_block(mUp2BufferPtrs.Get(c), inputs[c], nFrames);
       }
-    }
-    
-    if (mRate >= 4) {
-      for(auto c = 0; c < nInChans; c++) {
+      if (mRate >= 4) {
         mUpsampler4x.Get(c)->process_block(mUp4BufferPtrs.Get(c), mUp2BufferPtrs.Get(c), nFrames * 2);
       }
-    }
-    
-    if (mRate >= 8) {
-      for(auto c = 0; c < nInChans; c++) {
+      if (mRate >= 8) {
         mUpsampler8x.Get(c)->process_block(mUp8BufferPtrs.Get(c), mUp4BufferPtrs.Get(c), nFrames * 4);
       }
-    }
-    
-    if (mRate == 16) {
-      for(auto c = 0; c < nInChans; c++) {
+      if (mRate == 16) {
         mUpsampler16x.Get(c)->process_block(mUp16BufferPtrs.Get(c), mUp8BufferPtrs.Get(c), nFrames * 8);
       }
     }
     
     if (mRate == 1) {
       func(inputs, outputs, nFrames);
-    }
-    else {
+    } else {
       for (auto i = 0; i < mRate; i++) {
         for(auto c = 0; c < nInChans; c++) {
           mNextInputPtrs.Set(c, mInPtrLoopSrc->Get(c) + (i * nFrames));
-        }
-        for(auto c = 0; c < nOutChans; c++) {
           mNextOutputPtrs.Set(c, mOutPtrLoopSrc->Get(c) + (i * nFrames));
         }
         func(mNextInputPtrs.GetList(), mNextOutputPtrs.GetList(), nFrames);
       }
     }
     
-    if (mRate == 16) {
-      for(auto c = 0; c < nOutChans; c++) {
+    for(auto c = 0; c < nOutChans; c++) {
+      if (mRate == 16) {
         mDownsampler16x.Get(c)->process_block(mDown8BufferPtrs.Get(c), mDown16BufferPtrs.Get(c), nFrames * 8);
       }
-    }
-    
-    if (mRate >= 8) {
-      for(auto c = 0; c < nOutChans; c++) {
+      if (mRate >= 8) {
         mDownsampler8x.Get(c)->process_block(mDown4BufferPtrs.Get(c), mDown8BufferPtrs.Get(c), nFrames * 4);
       }
-    }
-    
-    if (mRate >= 4) {
-      for(auto c = 0; c < nOutChans; c++) {
+      if (mRate >= 4) {
         mDownsampler4x.Get(c)->process_block(mDown2BufferPtrs.Get(c), mDown4BufferPtrs.Get(c), nFrames * 2);
       }
-    }
-    
-    if (mRate >= 2) {
-      for(auto c = 0; c < nOutChans; c++) {
+      if (mRate >= 2) {
         mDownsampler2x.Get(c)->process_block(outputs[c], mDown2BufferPtrs.Get(c), nFrames);
       }
     }
